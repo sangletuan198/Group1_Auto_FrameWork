21acc97abe6cdd8c9f705fafce4eca24dec69662
Nghiem Van Long <88993999+oniyatos@users.noreply.github.com>
2022-12-09 12:50:32 +0700

    Update Add/Delete/Edit Assign

---
+++ Scenario_Team1_Auto/TestSetup/ProjectNUnitTestSetup.cs
@@ -0,0 +1,19 @@
+﻿using CoreFramework.NUnitTestSetup;
+using NUnit.Framework;
+
+namespace Scenario_Team1_Auto.TestSetup
+{
+    public class ProjectNUnitTestSetup : NUnitTestSetup
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _driver.Url = Constant.baseUrl;
+        }
+        [TearDown]
+        public void TearDown()
+        {
+        }
+    }
+}
+
